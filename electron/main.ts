91cc19678ebbcc654871102e20721f46e08891ba
dong <kouyidong99@163.com>
2024-01-03 00:02:02 +0800

    fix: 关闭自动打开控制头；增加 electron 镜像

--- electron/main.ts
+++ electron/main.ts
@@ -42,7 +42,7 @@ function createWindow() {
     win.loadFile(path.join(process.env.DIST, 'index.html'))
   }
 
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
